cb3043bdfd0d672ec813c4fa3b48ba3cf06d7201
Danniel Tung <dwftung@gmail.com>
2021-02-09 19:50:52 -0300

    worked on creating a manufacturer

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,2 +1,16 @@
 class ManufacturersController < ApplicationController
+  def create
+    @manufacturer = Manufacturer.new(manufacturer_params)
+    if @manufacturer.save
+      redirect_to root_path, notice: 'Fornecedor Adicionado!'
+    else
+      render 'pages/home'
+    end
+  end
+
+  private
+
+  def manufacturer_params
+    params.require(:manufacturer).permit(:name, :LG, :ICMS, :discount_1, :discount_2, :discount_3, :discount_4)
+  end
 end

5411a834f073244ebfc946e8ed8c83eab0df655c
Danniel Tung <dwftung@gmail.com>
2021-02-14 08:44:43 -0300

    worked on updating a field with another

--- app/controllers/price_lists_controller.rb
+++ app/controllers/price_lists_controller.rb
@@ -8,6 +8,13 @@ class PriceListsController < ApplicationController
   def create
     @price_list = PriceList.new(price_list_params)
     if @price_list.save
+      Sku.where(gross_price: nil).destroy_all
+      # a = Sku.where(price_list_id: @price_list.id).as_json
+      # raise
+      Sku.where(price_list_id: @price_list.id).update_all('net_price = gross_price * 2')
+      # raise
+      # a.each { |sku| sku["net_price"] = sku["gross_price"] * 2 }
+      # raise
       redirect_to manufacturer_path(@price_list.manufacturer_id), notice: 'Lista de Preços Criada!'
     else
       render 'pages/home'
@@ -20,19 +27,21 @@ class PriceListsController < ApplicationController
     params.require(:price_list).permit(:manufacturer_id, skus_attributes: [:gtin, :gross_price, :net_price, :sale_price])
   end
 
-  # def discount1
-  #   Manufacturer.find(params[:price_list][:manufacturer_id]).discount_1.nil? ? 0 : Manufacturer.find(params[:price_list][:manufacturer_id]).discount_1 / 100
-  # end
+  def discount1
+    Manufacturer.find(params[:price_list][:manufacturer_id]).discount_1.nil? ? 0 : Manufacturer.find(params[:price_list][:manufacturer_id]).discount_1 / 100
+  end
 
-  # def discount2
-  #   Manufacturer.find(params[:price_list][:manufacturer_id]).discount_2.nil? ? 0 : Manufacturer.find(params[:price_list][:manufacturer_id]).discount_2 / 100
-  # end
+  def discount2
+    Manufacturer.find(params[:price_list][:manufacturer_id]).discount_2.nil? ? 0 : Manufacturer.find(params[:price_list][:manufacturer_id]).discount_2 / 100
+  end
 
-  # def discount3
-  #   Manufacturer.find(params[:price_list][:manufacturer_id]).discount_3.nil? ? 0 : Manufacturer.find(params[:price_list][:manufacturer_id]).discount_3 / 100
-  # end
+  def discount3
+    Manufacturer.find(params[:price_list][:manufacturer_id]).discount_3.nil? ? 0 : Manufacturer.find(params[:price_list][:manufacturer_id]).discount_3 / 100
+  end
 
-  # def discount4
-  #   Manufacturer.find(params[:price_list][:manufacturer_id]).discount_4.nil? ? 0 : Manufacturer.find(params[:price_list][:manufacturer_id]).discount_4 / 100
-  # end
+  def discount4
+    Manufacturer.find(params[:price_list][:manufacturer_id]).discount_4.nil? ? 0 : Manufacturer.find(params[:price_list][:manufacturer_id]).discount_4 / 100
+  end
 end
+
+# params[:price_list][:skus_attributes]["0"][:gross_price]
--- app/controllers/skus_controller.rb
+++ app/controllers/skus_controller.rb
@@ -1,23 +1,23 @@
 class SkusController < ApplicationController
 
-  def new
-    @price_list = PriceList.find(params[:price_list_id])
-    @sku = Sku.new
-  end
+  # def new
+  #   @price_list = PriceList.find(params[:price_list_id])
+  #   @sku = Sku.new
+  # end
 
-  def create
-    @price_list = PriceList.find(params[:price_list_id])
-    @sku = Sku.new(sku_params)
-    if @sku.save
-      redirect_to manufacturer_path(@price_list.manufacturer_id), notice: 'Lista de Preços Criada!'
-    else
-      render 'pages/home'
-    end
-  end
+  # def create
+  #   @price_list = PriceList.find(params[:price_list_id])
+  #   @sku = Sku.new(sku_params)
+  #   if @sku.save
+  #     redirect_to manufacturer_path(@price_list.manufacturer_id), notice: 'Lista de Preços Criada!'
+  #   else
+  #     render 'pages/home'
+  #   end
+  # end
 
-  private
+  # private
 
-  def sku_params
-    params.require(:sku).permit(:gtin, :gross_price, :net_price, :sale_price)
-  end
+  # def sku_params
+  #   params.require(:sku).permit(:gtin, :gross_price, :net_price, :sale_price)
+  # end
 end
